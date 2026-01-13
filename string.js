c0c559b2a30a9961cbd82771062bde24e56cfc11
Tanishq Parashar <tanishqparashar123@gmail.com>
2026-01-13 11:41:20 +0530

    question2

--- string.js
+++ string.js
@@ -4,3 +4,10 @@ let str = "Js is the fun";
 let reversed = str.split(" ").reverse().join(" ");
 
 console.log(reversed);
+
+//Question 2
+function rotation(s1,s2){
+    if(s1.length !==s2.length) return false;
+    return (s1+s1).includes(s2);
+}
+console.log(rotation("ABCD","CDAB"));
