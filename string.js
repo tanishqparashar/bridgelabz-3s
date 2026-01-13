1c4166abb21bd6cb86daac6284abd816dd71d096
Tanishq Parashar <tanishqparashar123@gmail.com>
2026-01-13 22:29:41 +0530

    question3

--- string.js
+++ string.js
@@ -11,3 +11,23 @@ function rotation(s1,s2){
     return (s1+s1).includes(s2);
 }
 console.log(rotation("ABCD","CDAB"));
+
+// Question 3
+function runLengthEncode(str) {
+    if (str.length === 0) return "";
+
+    let result = "";
+    let count = 1;
+
+    for (let i = 1; i <= str.length; i++) {
+        if (str[i] === str[i - 1]) {
+            count++;
+        } else {
+            result += str[i - 1] + count;
+            count = 1;
+        }
+    }
+
+    return result;
+}
+console.log(runLengthEncode("aaabbc"));
